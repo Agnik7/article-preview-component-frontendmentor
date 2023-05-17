ec0c85d76361f046cd7cb51ff35a8dc93ab7befe
Agnik Bakshi <77234005+Agnik7@users.noreply.github.com>
2023-05-17 11:26:21 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const share = document.querySelector("#share");
+const share_icon = document.querySelectorAll(".share_icon");
+
+for (let i = 0; i < share_icon.length; i++) {
+  share_icon[i].addEventListener("click", () => {
+    share.classList.value == "share slide-out-bottom"
+      ? (share.classList.value = "share slide-top")
+      : (share.classList.value = "share slide-out-bottom");
+  });
+}
